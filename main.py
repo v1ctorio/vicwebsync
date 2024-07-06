360cf7478ce850e384999953ed4959da9d94f799
vic <74506415+v1ctorio@users.noreply.github.com>
2024-07-06 23:06:39 +0200

    Make telegram bot and database like actually work

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 import os
 import uuid
 import pymongo
+import dotenv
 
-db = pymongo.MongoClient("mongodb://localhost:27017/")["vicweb"]
+dotenv.load_dotenv()
+
+db = pymongo.MongoClient(os.getenv("MONGOURL"))["vicweb"]
 postsCollection = db["posts"]
-
+print("Connected to database!", postsCollection)
 def main() -> None:
     tgbot = Application.builder().token(os.getenv("TG_TOKEN")).build()
-    tgbot.add_handler(MessageHandler(filters.ALL, message))
+    tgbot.add_handler(MessageHandler(filters.ALL, handleMessage))
+    tgbot.run_polling(allowed_updates=Update.MESSAGE)
+    print("Telegram bot started!")
 
-def message(update: Update, context: ContextTypes) -> None:
-    if (update.message.chat.id != os.getenv("TG_CHAT_ID")):
+async def handleMessage(update: Update, context: ContextTypes) -> None:
+    print(f"New message recived! {update.message.chat.id}" )
+    if (update.message.chat.id != int(os.getenv("TG_CHAT_ID"))):
+        print(os.getenv("TG_CHAT_ID"))
         return
     print("New channel message recived!")
     Iid = str(uuid.uuid4())
@@ -22,13 +29,14 @@ def message(update: Update, context: ContextTypes) -> None:
 
     if (dbdata == None):
         print (f"Message not found in database! Adding message to database with id: {Iid}")
-        postsCollection.insert_one({"id": Iid, "content": Mcontent})
+        postsCollection.insert_one({"id": Iid, "content": Mcontent,"source": "telegram"})
         print("Post added to database!")
         return 
     print("Message skipped (already in database)!")
 
     
 
+print(__name__)
 
 if (__name__ == "__main__"):
     main()
--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 let
   # We pin to a specific nixpkgs commit for reproducibility.
   # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
-  pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> {
+    config.allowUnfree = true;
+  };
 in pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
