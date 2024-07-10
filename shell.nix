a58a45678238238774a97fc15e6d27baf0073bf5
vic <74506415+v1ctorio@users.noreply.github.com>
2024-07-10 03:10:49 +0200

    Update nix shell to rust

--- shell.nix
+++ shell.nix
@@ -1,18 +1,11 @@
 let
-  # We pin to a specific nixpkgs commit for reproducibility.
-  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  
   pkgs = import <nixpkgs> {
     config.allowUnfree = true;
   };
 in pkgs.mkShell {
   packages = [
-    (pkgs.python3.withPackages (python-pkgs: [
-      # select Python packages here
-      python-pkgs.python-telegram-bot
-      python-pkgs.requests
-      python-pkgs.python-dotenv
-      python-pkgs.pymongo
-      python-pkgs.flask
-    ]))
+  rustc
+  cargo
   ];
 }
