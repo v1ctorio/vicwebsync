b5323dbcdc2e7de08d8377ac7cc4961ebab595ed
vic <74506415+v1ctorio@users.noreply.github.com>
2024-07-10 12:53:20 +0200

    adding webserver??

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,29 @@ use frankenstein::TelegramApi;
 use frankenstein::UpdateContent;
 
 use mongodb::{ 
-	bson::{Document, doc},
-	sync::{Client, Collection} 
-};
+    bson::{Document, doc, oid},
+    Client,
+    Collection,
+    options::ClientOptions,
+    options::ResolverConfig,
+}; 
 use rouille::Request;
 use rouille::Response;
 
-fn main() {
+use std::thread;
+use chrono;
+
+#[tokio::main]
+async fn main()  -> Result<(), Box<dyn std::error::Error>>{
 
 
-    rouille::start_server("0.0.0.0:80", move |request| {
-        Response::text("hello cHAT")
+    let webserver = thread::spawn(|| {
+        rouille::start_server("0.0.0.0:5432", move |req| {
+            handle_req(&req)
+        });
     });
-
     dotenv::dotenv().ok();
+    
     let tgtoken = dotenv::var("TG_TOKEN").unwrap();
     let tgchannelid = dotenv::var("TG_CHAT_ID").unwrap();
     let tgapi = Api::new(&tgtoken);
@@ -28,9 +37,12 @@ fn main() {
     let upb = GetUpdatesParams::builder();
     let mut update_params = upb.clone().build();
 
-
-    let mongouri = dotenv::var("MONGOURL").unwrap();
-    let mongoclient = Client::with_uri_str(&mongouri)?;
+    let mongouri = "mongodb://idk";
+    //let mongouri = dotenv::var("MONGOURL").unwrap();
+    // let mongooptions =
+    // ClientOptions::parse(&mongouri, ResolverConfig::cloudflare())
+    //    .await?;
+    let mongoclient = Client::with_uri_str(mongouri).await?;
     let db = mongoclient.database("vicweb");
     let posts_collection: Collection<Document> = db.collection("posts");
 
@@ -43,18 +55,25 @@ fn main() {
                     if let UpdateContent::Message(message) = update.content {
                         let chatid = message.chat.id;
                         let text = message.text.unwrap();
-                        println!("Chat id: {:?}, Text: {:?}", chatid, text);
+                        println!("Chat id: {:?}, Text: {:?}", chatid, &text);
                         
                         //(if chat id is tgchannel id)
                         if chatid == tgchannelid.parse::<i64>().unwrap() {
-                            let existing_post = posts_collection.find_one(doc!{"text": text}, None)?;
-                            if (existing_post.is_none()) {
+                            let existing_post = posts_collection.find_one(doc!{"text": &text}).await?;
+                            if existing_post.is_none() {
                                 let post = doc! {
-                                    "text": text,
-                                    "created_at": chrono::Utc::now(),
-                                    "_id": bson::oid::ObjectId::new(),
+                                    "text": &text,
+                                    "created_at": chrono::Utc::now().timestamp(),
+                                    "_id": oid::ObjectId::new(),
                                 };
-                                posts_collection.insert_one(post, None)?;
+                                match posts_collection.insert_one(post.clone(),None).await {
+                                    Ok(resultingPost) => {
+                                        println!("New post added to db: {:?}", text);
+                                    }
+                                    Err(e) => {
+                                        println!("Error adding post to db: {:?}", e);
+                                    } 
+                                };
                                 println!("New post added to db: {:?}", text);
                             } else {
                                 println!("Ignoring post sin it already exists in db: {:?}", text);
@@ -74,4 +93,8 @@ fn main() {
             }
         }
     }
+}
+
+fn handle_req(req: &Request) -> Response {
+    Response::text("hello cHAT")
 }
