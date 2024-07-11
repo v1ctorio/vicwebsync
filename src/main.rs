c5edeee554ef6696dca4ff2ee44bf6c73d9f54f4
vic <74506415+v1ctorio@users.noreply.github.com>
2024-07-11 13:51:05 +0200

    add mongo so far

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use dotenv;
 
 use mongodb::{ 
     bson::{Document, doc, oid},
-    sync::{Client, Collection},
+    Client, Collection,
     error::Result,
     error::Error
 }; 
@@ -17,32 +17,52 @@ use teloxide::prelude::*;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+use serde::{ Deserialize, Serialize };
+#[derive(Serialize, Deserialize, Debug)]
+struct Post {
+    date: String,
+    content: String,
+}
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
+    let mongo_client = Client::with_uri_str(&std::env::var("MONGO_URI").expect("MONGO_URI must be set"))
+    .await
+    .expect("Failed to initialize standalone client.");
+    let db: Collection<Document> = mongo_client.database("vicweb").collection("posts");
+
+    let collection_arc = Arc::new(db);
+    info!("Connected to the database");
+
     info!("Starting the server");
-    let web_server = tokio::spawn(start_web_server());
-    let telegram_bot = tokio::spawn(start_telegram_bot());
+    let web_server = tokio::spawn(start_web_server(collection_arc.clone()));
+    let telegram_bot = tokio::spawn(start_telegram_bot(collection_arc.clone()));
 
     let _ = tokio::try_join!(web_server, telegram_bot);
 
 }
 
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>> {
+async fn handle_request(req: Request<Body>,mongo: Arc<Collection<Document>>) -> Result<Response<Body>, Infallible> {
+
+    let data = mongo.find(None, None).await.expect("Failed to execute find.");
     info!("Received a request to the webserver, {:?}", req.uri());
+    info!("Data from the database: {:?}", data);
     Ok(Response::new(Body::from("Hello chat")))
 }
 
-async fn start_web_server() {
+async fn start_web_server(mongo: Arc<Collection<Document>>) {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     let make_svc = make_service_fn(|_conn| {
+        let mongo = mongo.clone();
         async {
-            Ok::<_, Infallible>(service_fn(handle_request))
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handle_request(req, mongo.clone())
+            }))
         }
     });
 
@@ -55,7 +75,7 @@ async fn start_web_server() {
     }
 }
 
-async fn start_telegram_bot() {
+async fn start_telegram_bot(mongo: Arc<Collection<Document>>) {
     let bot = Bot::from_env();
     info!("Bot successfully created");
     teloxide::repl(bot, |_bot: Bot, msg: Message| async move {
